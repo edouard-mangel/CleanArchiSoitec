4ca88e97833de386e11f70a17a21b628a5bf9cc0
morganbrault <morgan.brault@soitec.com>
2025-09-30 15:45:19 +0200

    Commit 5

---
+++ UnitTests/ScheduleWriterFake.cs
@@ -0,0 +1,14 @@
+﻿using Domain;
+
+namespace UnitTests
+{
+    internal class ScheduleWriterFake : IScheduleWriter
+    {
+        public List<Schedule> Schedules { get; internal set; } = new();
+
+        public void ExportSchedule(Schedule schedule)
+        {
+            Schedules.Add(schedule);
+        }
+    }
+}
